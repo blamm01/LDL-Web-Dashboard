b94868b70d52e7777fe38598834b5fad0cb5340a
LamHanoi10 <77109479+LamHanoi10@users.noreply.github.com>
2022-07-08 14:07:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     "loginLink": "https://discord.com/api/oauth2/authorize?client_id=739280940316098610&redirect_uri=https%3A%2F%2Fldl-web-dashboard.herokuapp.com2Fapi%2Fauth%2Fcallback&response_type=code&scope=identify%20guilds",
     "inviteLink": "https://discord.com/api/oauth2/authorize?client_id=739280940316098610&permissions=536098499710&redirect_uri=https%3A%2F%2Fldl-web-dashboard.herokuapp.com2Fapi%2Fauth%2Fcallback&response_type=code&scope=bot%20identify%20guilds%20applications.commands",
     "port": 3001,
-    "callback": "https://ldl-web.lamhanoi10.repl.co/api/auth/callback",
+    "callback": "https://ldl-web-dashboard.herokuapp.com/api/auth/callback",
     "scope": ["identify", "guilds"],
     "tokenAge": 2629746000,
     "domainLink": "https://ldl-web-dashboard.herokuapp.com/",
