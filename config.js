1ea14b161c0d6c6089c1ecf635bba0dc5a02fbca
LamHanoi10 <77109479+LamHanoi10@users.noreply.github.com>
2022-07-08 14:05:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
     "callback": "https://ldl-web.lamhanoi10.repl.co/api/auth/callback",
     "scope": ["identify", "guilds"],
     "tokenAge": 2629746000,
-    "domainLink": "https://ldl-web.lamhanoi10.repl.co",
+    "domainLink": "https://ldl-web-dashboard.herokuapp.com/",
     "prefix": "l!"
 }
