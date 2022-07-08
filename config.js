d91f13156ea7d9b242b42de9e23311a08aaa7bbc
LamHanoi10 <77109479+LamHanoi10@users.noreply.github.com>
2022-07-08 14:05:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "loginLink": "https://discord.com/api/oauth2/authorize?client_id=991263295623405589&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fcallback&response_type=code&scope=guilds%20identify",
-    "inviteLink": "https://discord.com/api/oauth2/authorize?client_id=991263295623405589&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fcallback&response_type=code&scope=bot%20guilds%20identify%20applications.commands",
-    "port": 3000,
-    "callback": "https://ldl-web-dashboard.herokuapp.com/api/auth/callback",
+    "loginLink": "https://discord.com/api/oauth2/authorize?client_id=739280940316098610&redirect_uri=https%3A%2F%2Fldl-web-dashboard.herokuapp.com2Fapi%2Fauth%2Fcallback&response_type=code&scope=identify%20guilds",
+    "inviteLink": "https://discord.com/api/oauth2/authorize?client_id=739280940316098610&permissions=536098499710&redirect_uri=https%3A%2F%2Fldl-web-dashboard.herokuapp.com2Fapi%2Fauth%2Fcallback&response_type=code&scope=bot%20identify%20guilds%20applications.commands",
+    "port": 3001,
+    "callback": "https://ldl-web.lamhanoi10.repl.co/api/auth/callback",
     "scope": ["identify", "guilds"],
     "tokenAge": 2629746000,
-    "domainLink": "hhttps://ldl-web-dashboard.herokuapp.com",
+    "domainLink": "https://ldl-web.lamhanoi10.repl.co",
     "prefix": "l!"
 }
