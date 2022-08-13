afb11a2a8569b779fe5ac724e01298d08eeb1fa1
LamHanoi10 <blam2010@outlook.com.vn>
2022-08-13 17:18:43 +0700

    Edited config.ts

--- config.ts
+++ config.ts
@@ -1,14 +1,14 @@
 export default {
-  JWT_TOKEN: '3333333',
+  JWT_TOKEN: 'n3BYVFlKR_sErWKq_I0uXE2pB29iqbD8',
   PORT: 3000,
 
-  CLIENT_TOKEN: "MTAwMjczMDQ1ODY0OTQ3NzI1MA.G0Y_CF.QzE8jfXnu87WAZ1QJrzLa4x_aXskw35lOvB8-k",
-  CLIENT_ID: '1002730458649477250',
+  CLIENT_TOKEN: "MTAwNDAzNjY0NDI2MjkxMjAxMA.GIMHtz.Shzg3Cp1Y-FM5nZYPCJFea7CVBZOoEI3tfnhvM",
+  CLIENT_ID: '1004036644262912010',
   CLIENT_SECRET: '4VBZ_syPuJEEk77oVLsYDzCSXWCFfnsD',
   CALLBACK_URL: 'http://localhost:3000/api/auth/callback',
   SCOPES: ['identify', 'guilds'],
   MAX_AGE: 86400000,
 
   MONGO_URI:
-    'mongodb+srv://Websites:websitedb@db-for-multi-bot.iymce.mongodb.net/example_dashboard_bot?retryWrites=true&w=majority',
+    'mongodb+srv://PewZew:20042007@cluster0.zulbe7d.mongodb.net/?retryWrites=true&w=majority',
 };
