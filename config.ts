3d61f3df761dc11da629e12bb47754b27c73ceec
LamHanoi10 <77109479+LamHanoi10@users.noreply.github.com>
2022-12-10 17:05:32 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,9 +2,9 @@ export default {
   JWT_TOKEN: 'n3BYVFlKR_sErWKq_I0uXE2pB29iqbD8',
   PORT: 12221,
 
-  CLIENT_TOKEN: "MTAwNDAzNjY0NDI2MjkxMjAxMA.GIMHtz.Shzg3Cp1Y-FM5nZYPCJFea7CVBZOoEI3tfnhvM",
-  CLIENT_ID: '1004036644262912010',
-  CLIENT_SECRET: 'Qh_GGky3UFyAMeLXmStTqqwz5Yk4JFna',
+  CLIENT_TOKEN: "ODUwOTk2MDAwMTYzNDk1OTQ4.Gnh17e.CJRoc-gx3njbaYZgC8G9OoXRJ0rVKFgRSL6_Nc",
+  CLIENT_ID: '850996000163495948',
+  CLIENT_SECRET: '8qVXED8CGL3aPn-c863EbAI4fG1uZAoR',
   CALLBACK_URL: 'https://ldl-web-dashboard-production.up.railway.app/api/auth/callback',
   SCOPES: ['identify', 'guilds'],
   MAX_AGE: 86400000,
